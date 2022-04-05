be4d68dbb139047ceae3d6f022d18364d1266ae3
DESKTOP-JUGOZ\test1 <ligmh@qq.com>
2022-04-05 09:29:19 +0800

    1）影视播放时间改为用setInterval实现 2）增加暂停/继续的api

--- server.js
+++ server.js
@@ -5,39 +5,69 @@ const app = express()
 app.use('/static', express.static('static'));
 
 var count = 0
+var time = 0 // 播放了多久了，单位是秒
 var startTime = new Date().getTime()
-var initStartTime = startTime
 
-app.get('/',function(req,res){
-    res.sendFile( __dirname + "/static/index.html" );
+var clock = setInterval(() => { // 开始计时
+    time++
+}, 1000);
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + "/static/index.html");
 })
 
 
-app.get('/getPlayInfo',function(req,res){
+app.get('/pause', function (req, res) {
+    clearInterval(clock)
+    clock = null
+    console.log('paused');
+    res.send('pause success')
+})
+
+app.get('/resume', function (req, res) {
+    clock = setInterval(() => { // 开始计时
+        time++
+    }, 1000);
+    console.log('resumed');
+    res.send('resume success')
+})
+
+app.get('/pauseOrResume', function (req, res) {
+    if(!clock){
+        clock = setInterval(() => { // 开始计时
+            time++
+        }, 1000);
+        console.log('resumed');
+    } else {
+        clearInterval(clock)
+        clock = null
+        console.log('paused');
+    }
+    res.send('success')
+})
+
+app.get('/getPlayInfo', function (req, res) {
     count++
-    var myDate = new Date()
-    var res_string = 'count:'+count+', time: '+myDate.getHours()+':'+myDate.getMinutes()+':'+myDate.getSeconds()+'+'+myDate.getMilliseconds()
-    var playTime = Math.ceil((myDate.getTime() - startTime)/1000) // 影片放映了多久
     var response = {
-        "time":playTime,
-        "test":'just for test',
-        "sysInfo":res_string
+        "time": time,
+        "count": count,
+        "sysTime": (new Date()).getTime()
     };
     console.log(response);
     res.end(JSON.stringify(response));
 })
 
-app.get('/forward5Minute',function(req,res){
-    startTime+=300000
-    res.send('nihao')
+app.get('/forward', function (req, res) {
+    startTime += 300000
+    res.send('success')
 })
 
-app.get('/backward5Minute',function(req,res){
-    startTime-=300000
-    res.send('nihao')
+app.get('/backward', function (req, res) {
+    startTime -= 300000
+    res.send('success')
 
 })
 
-const server = app.listen(process.env.PORT || 5000,function(){
-    console.log('5000端口监听');
+const server = app.listen(process.env.PORT || 5000, function () {
+    console.log('listening port 5000');
 })
